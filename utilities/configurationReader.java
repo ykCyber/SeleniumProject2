d33ba93e65ad3fff463d7f2ebd2d25f491b0aae2
Karahan2002 <66959328+Karahan2002@users.noreply.github.com>
2021-07-27 10:47:14 +0200

    Add files via upload

---
+++ utilities/configurationReader.java
@@ -0,0 +1,27 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class configurationReader {
+    private static Properties properties;
+
+    static {
+        try{
+            //what file to read
+            String path = "configuration.properties";
+            //read the file into java, finds the file using the string path
+            FileInputStream input = new FileInputStream(path);
+            // properties --> class that store properties in key/value format
+            properties = new Properties();
+            //the values from the file input is loaded/fed into the properties object
+            properties.load(input);
+            input.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static String get(String ketName){
+        return properties.getProperty(ketName);
+    }
+}
